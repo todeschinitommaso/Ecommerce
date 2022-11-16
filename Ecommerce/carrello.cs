0c059593ee4d68919bf265b69d2a7fbd8d1870d5
Tommaso Todeschini <tommaso.todeschini05@gmail.com>
2022-11-16 16:03:02 +0100

    Aggiungere i file di progetto.

---
+++ Ecommerce/carrello.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    internal class carrello
+    {
+
+        private string _id;
+
+        string[] prodotto = new string[100];
+
+        private int counter;
+
+        public carrello(string id)
+        {
+            Id = id;
+
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+
+        }
+        
+        public void Aggiungi(prodotto p)
+        {
+            p.Id = prodotto[counter];
+            counter++;
+
+        }
+
+        public void Rimuovi(prodotto p)
+        {
+            p.Id = prodotto[counter];
+            counter--;
+
+        }
+
+        public int getProdotti()
+        {
+            return counter;
+
+        }
+
+        public void Svuota()
+        {
+            counter = 0;
+
+        }
+
+    }
+}
---
+++ Ecommerce/prodotto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    internal class prodotto
+    {
+        private string _id;
+
+        private string _nome;
+
+        private string _produttore;
+
+        private float _prezzo;
+
+        private string _descrizione;
+
+        public prodotto(string id, string nome, string produttore, string descrizione)
+        {
+            Id = id;
+
+            Nome = nome;
+
+            Produttore = produttore;
+
+            Descrizione = descrizione;
+
+        }
+
+        public string Id
+        {
+            get { return _id; }
+
+            set { _id = value; }
+
+        }
+        public string Nome
+        {
+            get { return _nome; }
+
+            set { _nome = value; }
+
+        }
+
+        public string Produttore
+        {
+            get { return _produttore; }
+
+            set { _produttore = value; }
+
+        }
+        public string Descrizione
+        {
+            get { return _descrizione; }
+
+            set { _descrizione = value; }
+
+        }
+
+        public void setPrezzo(float prezzo)
+        {
+            this._prezzo = prezzo;
+
+        }
+    }
+}
